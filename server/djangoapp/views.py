5369ffbf71783c8b295ec01d8b0dabd575e152a8
asernesto <alfredo_ernesto@hotmail.com>
2022-12-23 16:31:15 -0500

    updated log in and registration pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -47,8 +47,6 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/user_login.html', context)
 
-
-
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     context = {}
@@ -59,8 +57,33 @@ def logout_request(request):
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
     context = {}
+    # If it is a GET request, just render the registration page
     if request.method == 'GET':
         return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:about")
+        else:
+            return render(request, 'djangoapp/registration.html', context)
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
